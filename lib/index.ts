9228a414187210be6439f49b94d40bd57042a46f
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2022-01-14 10:19:10 +0100

    feat: add dd eks-ssp-addon codebase

--- lib/index.ts
+++ lib/index.ts
@@ -1,54 +1,65 @@
-import { ManagedPolicy } from '@aws-cdk/aws-iam';
-import { Construct } from '@aws-cdk/core';
-import * as ssp from '@aws-quickstart/ssp-amazon-eks';
+import { ClusterInfo } from "@aws-quickstart/ssp-amazon-eks";
+import { Construct } from "@aws-cdk/core";
 import { HelmAddOn, HelmAddOnProps, HelmAddOnUserProps } from '@aws-quickstart/ssp-amazon-eks/dist/addons/helm-addon';
+import merge from "ts-deepmerge";
 
-export interface MyFluentBitAddOnProps extends HelmAddOnUserProps {
-    cloudWatchRegion: string
+export interface DatadogAddOnProps extends HelmAddOnUserProps {
+    /**
+     * Your Datadog API key
+     */
+    apiKey?: string;
+
+    /**
+     * Datadog APP key required to use metricsProvider.
+     * If you are using clusterAgent.metricsProvider.enabled = true, you must set a Datadog application key for read access to your metrics.
+     */
+    appKey?: string;
+
+    /**
+     * Use existing Secret which stores API key instead of creating a new one.
+     * The value should be set with the `api-key` key inside the secret.
+     * If set, this parameter takes precedence over "apiKey".
+     */
+    apiKeyExistingSecret?: string;
+
+    /**
+     * Use existing Secret which stores APP key instead of creating a new one.
+     * The value should be set with the `app-key` key inside the secret.
+     * If set, this parameter takes precedence over "appKey".
+     */
+    appKeyExistingSecret?: string;
 }
 
-
-export const defaultProps: HelmAddOnProps & MyFluentBitAddOnProps = {
-    chart: 'aws-for-fluent-bit',
-    cloudWatchRegion: 'us-east-1',
-    name: 'my-addon',
-    namespace: 'kube-system',
-    release: 'ssp-addon-myextension-fluent-bit',
-    version: '0.1.11',
-    repository: 'https://aws.github.io/eks-charts',
+export const defaultProps: HelmAddOnProps & DatadogAddOnProps = {
+    chart: "datadog",
+    name: "datadog-addon",
+    namespace: "default",
+    release: "datadog",
+    repository: "https://helm.datadoghq.com",
+    version: "2.28.13",
     values: {}
-}
+};
 
-export class MyFluentBitAddOn extends HelmAddOn {
+export class DatadogAddOn extends HelmAddOn {
 
-    readonly options: MyFluentBitAddOnProps;
+    readonly options: DatadogAddOnProps;
 
-    constructor(props: MyFluentBitAddOnProps) {
+    constructor(props: DatadogAddOnProps) {
         super({...defaultProps, ...props});
-        this.options = this.props as MyFluentBitAddOnProps;
+        this.options = this.props as DatadogAddOnProps;
     }
 
-    deploy(clusterInfo: ssp.ClusterInfo): void | Promise<Construct> {
-        const serviceAccountName = 'aws-for-fluent-bit-sa';
-        const sa = clusterInfo.cluster.addServiceAccount('my-aws-for-fluent-bit-sa', {
-            name: serviceAccountName,
-            namespace: this.props.namespace
-        });
+    deploy(clusterInfo: ClusterInfo): Promise<Construct> {
+        let values = merge({
+            datadog: {
+                apiKey: this.options.apiKey,
+                appKey: this.options.appKey,
+                apiKeyExistingSecret: this.options.apiKeyExistingSecret,
+                appKeyExistingSecret: this.options.appKeyExistingSecret
+            }
+        }, this.options.values ?? {})
 
-        // Cloud Map Full Access policy.
-        const cloudWatchAgentPolicy = ManagedPolicy.fromAwsManagedPolicyName("CloudWatchAgentServerPolicy");
-        sa.role.addManagedPolicy(cloudWatchAgentPolicy);
-        
-        const chart = this.addHelmChart(clusterInfo, {
-            serviceAccount: {
-                create: false,
-                name: serviceAccountName
-            },
-            cloudWatch: {
-                region: this.options.cloudWatchRegion
-            } 
-        });
-        chart.node.addDependency(sa);
+        const chart = this.addHelmChart(clusterInfo, values);
 
         return Promise.resolve(chart);
     }
